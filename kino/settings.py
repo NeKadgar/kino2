04c7c0487e6d32831e27b625262d71d537f898db
Bonest Bead <maximsheleh@gmail.com>
2019-07-28 21:47:43 +0300

    das

--- kino/settings.py
+++ kino/settings.py
@@ -125,7 +125,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/home/maxim/kino2/kino/static'
 STATIC_URL = '/static/'
 CORS_ORIGIN_ALLOW_ALL = True
 
