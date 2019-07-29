d79ebc90b5de84ebdd5763f2cfb393ed0e89459d
Bonest Bead <maximsheleh@gmail.com>
2019-07-29 10:15:37 +0300

    fsa

--- kino/settings.py
+++ kino/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l6$$x8wdq@ee!+rx6b9_r7t=i7dt6_mskcsh(7#@^9-xqqv1om'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
